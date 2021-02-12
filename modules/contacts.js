59257731a9ee2ff7d1471ac105d0de77a7ac3e1d
Vadym <vad.evlanov@gmail.com>
2021-02-12 17:29:10 +0200

    add functions

--- modules/contacts.js
+++ modules/contacts.js
@@ -1,23 +1,56 @@
 import * as fs from 'fs/promises';
-import { pathToFileURL } from 'url';
+import path from 'path';
+import getFileDirName from '../lib/dirname.js';
+import { handleError } from '../lib/handlerror.js';
 
-const contactsPath = pathToFileURL('../db/contacts.json');
-console.log(contactsPath.pathname);
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  fs.readFile(contactsPath);
+const { __dirname } = getFileDirName(import.meta.url);
+const contactsPath = path.join(__dirname, '..', 'db', 'contacts.json');
+
+async function listContacts() {
+  try {
+    const res = await fs.readFile(contactsPath);
+    console.table(JSON.parse(res));
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const res = await fs.readFile(contactsPath);
+    const data = JSON.parse(res);
+    const contact = data.filter(contact => contact.id === contactId);
+    console.table(...contact);
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const res = await fs.readFile(contactsPath);
+    const data = JSON.parse(res);
+    const contacts = data.filter(contact => contact.id !== contactId);
+    if (contacts.length === data.length) {
+      console.log('No contacts with such id');
+      return;
+    } else {
+      fs.writeFile(contactsPath, JSON.stringify(contacts));
+      console.log('Contact removed!');
+    }
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const res = await fs.readFile(contactsPath);
+    const data = JSON.parse(res);
+    const contacts = data.filter(contact => contact.id !== contactId);
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 export default {
